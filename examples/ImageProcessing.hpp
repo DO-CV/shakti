75e138819f65cbcc4f66ca5eed729ed15f94d313
David OK <david.ok8@gmail.com>
2015-08-06 17:32:30 +0100

    MAINT: use texture object for gradient computation.

--- examples/ImageProcessing.hpp
+++ examples/ImageProcessing.hpp
@@ -4,79 +4,34 @@
 #include <DO/Shakti/MultiArray/Offset.hpp>
 
 
-//! Question: understand whether there is a performance penalty when adding
-//! more parameters to the CUDA kernel function.
-/*!
-    Presumably the '__constant__' keyword hints at this problem.
- */
+namespace DO { namespace Shakti {
 
-//! Avoid warp divergence.
-//! The key idea is to partition the data when conditional statement appears.
-//! However, profiling is needed in order to find out whether a specific part of
-//! CUDA code is worth optimizing.
+  texture<float, 2> in_texture;
+  texture<float, 2> out_texture;
 
-template <typename T, int N>
-__global__
-void init(T *data, int size)
-{
-  int i = DO::Shakti::offset<N>();
-  data[i] = 1;
-}
 
-template <typename T, int N>
-__global__
-void gradient(const T *src, T *dst, int size, DO::Shakti::Vector<int, N> strides)
-{
-  int i = DO::Shakti::offset<N>();
-
-  DO::Shakti::Vector<T, N> grad_f;
-#pragma unroll
-  for (int i = 0; i < N; ++i)
+  template <typename T>
+  __global__
+  void gradient(T *dst, int size)
   {
-    int ip = i + strides(i);
-    int im = i - strides(i);
-    if (ip >= size)
-      ip = i;
-    if (im < 0)
-      im = i;
-    grad_f(i) = (src[ip] - src[im]) / 2;
+    const int i{ offset<2>() };
+    const Vector2i p{ coords<2>() };
+    if (i >= size)
+      return;
+
+    auto u_x = tex2D(in_texture, p(0)+1, p(1)) - tex2D(in_texture, p(0)-1, p(1));
+    auto u_y = tex2D(in_texture, p(0), p(1)+1) - tex2D(in_texture, p(0), p(1)-1);
+    dst[i] = sqrt(u_x*u_x + u_y*u_y);
   }
 
-  dst[i] = sqrt(grad_f.dot(grad_f));
-}
-
-template <typename T, int N>
-__global__
-void laplacian(const T *src, T *dst, int size, DO::Shakti::Vector<int, N> strides)
-{
-  int i = DO::Shakti::offset<N>();
-
-  dst[i] = -2*N*src[i];
-#pragma unroll
-  for (int i = 0; i < N; ++i)
+  template <typename T, int N>
+  __global__
+  void laplacian(const T *src, T *dst, int size)
   {
-    int ip = i + strides(i);
-    int im = i - strides(i);
-    if (ip >= size)
-      ip = i;
-    if (im < 0)
-      im = i;
-    dst[i] = src[ip] + src[im];
-  }
-}
+    int i = DO::Shakti::offset<N>();
 
-//template <typename T, int N>
-//__global__
-//void hessian(const T *src, Matrix<T, N, N> *dst, int size,
-//             Shakti::Vector<int, N> strides)
-//{
-//  int off = offset<N>();
-//
-//  for (int i = 0; i < N; ++i)
-//  {
-//    for (int j = i; j < N; ++j)
-//    {
-//      dst[off](i,j) = ....
-//    }
-//  }
-//}
+    dst[i] = -2 * N*src[i];
+  }
+
+} /* namespace Shakti */
+} /* namespace DO */
