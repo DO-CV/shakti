9df6fd35d5b7133ae250b534494b2e8b304ceffa
David OK <david.ok8@gmail.com>
2015-10-13 11:47:14 +0100

    ENH: add build script for Shakti.

---
+++ build-shakti.sh
@@ -0,0 +1,9 @@
+mkdir ../shakti-build-shared
+cd ../shakti-build-shared
+cmake ../shakti \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DSHAKTI_BUILD_SHARED_LIBS=ON \
+  -DSHAKTI_BUILD_TESTS=ON \
+  -DSHAKTI_BUILD_SAMPLES=ON
+
+make -j`nproc` && make test && make package
