d2d879a4451d37a6ac2ba12e3612ce2370fdb36f
David OK <david.ok8@gmail.com>
2015-11-17 20:54:16 +0000

    MAINT: explicitly specify CUDA version in build script.

--- build-shakti.sh
+++ build-shakti.sh
@@ -2,6 +2,7 @@ mkdir ../shakti-build-shared
 cd ../shakti-build-shared
 cmake ../shakti \
   -DCMAKE_BUILD_TYPE=Release \
+  -DCUDA_TOOLKIT_ROOT_DIR=/usr/local/cuda-7.5 \
   -DSHAKTI_BUILD_SHARED_LIBS=ON \
   -DSHAKTI_BUILD_PYTHON_BINDINGS=ON \
   -DSHAKTI_BUILD_TESTS=ON \
